e9cbeb14fc8a0bd1caa83283a120356de8e5f15b
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2024-12-13 15:17:07 +0530

    today is enough for now , will do coding after this.

--- first.js
+++ first.js
@@ -1,5 +1,2 @@
-// Create a number variable num with some value. Now, print “good” if the number is divisible by 10 and print “bad” if it is not
-let num = 100
-if(num%10==0)
-  console.log("Good , This number is divisible by 10")
-else if("Bad, This number is not divisible by 10")
+alert("This is a js practice file.")
+console.log("how are you console bhai.")
