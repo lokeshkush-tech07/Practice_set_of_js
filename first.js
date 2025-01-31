c7e1fd5d16edad61b4cb07966e256b84306fc334
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2025-01-31 13:42:12 +0530

    I have done practice 1

--- first.js
+++ first.js
@@ -1,2 +1,4 @@
-alert("This is a js practice file.")
-console.log("how are you console bhai.")
+//alert("This is a js practice file.")//
+//console.log("how are you console bhai.");
+let cart_quantity = 6;
+document.querySelector('#cart_items').innerText = `Your bag has ${cart_quantity} items.`;
